d75e616b681bd5a3208960a86dc869af6698e79f
Edgaras Žuklys <1311288@activedir.vu.lt>
2022-03-19 19:46:41 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,6 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-// messageOne.textContent = 'From js';
-
-
-
 weatherForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const location = search.value;
@@ -14,7 +10,7 @@ weatherForm.addEventListener('submit', (event) => {
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
 
-  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error){
         messageOne.textContent = data.error; 
@@ -22,8 +18,6 @@ weatherForm.addEventListener('submit', (event) => {
       } else {
         messageOne.textContent = data.location; 
         messageTwo.textContent = data.forecast.description; 
-        // console.log(data.location);
-        // console.log(data.forecast);
       };
     });
   });
